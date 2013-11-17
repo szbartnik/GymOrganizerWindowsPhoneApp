5b367cdd9d793a6d06e18eeba0f59a36980d2d61
Szymon - Desktop <szymbar099@student.polsl.pl>
2013-11-17 23:16:43 +0100

    some changes in user experiences in AddNewExercisePage.xaml

--- FitAndGym/View/AddNewExercisePage.xaml.cs
+++ FitAndGym/View/AddNewExercisePage.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using FitAndGym.Infrastructure;
 using FitAndGym.Models;
 using FitAndGym.Resources;
+using FitAndGym.ViewModels;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
@@ -15,13 +16,31 @@ namespace FitAndGym.View
 {
     public partial class AddNewExercisePage : PhoneApplicationPage
     {
+        private const int MAX_NUM_OF_SETS = 100;
+        private const int MAX_NUM_OF_REPS = 500;
+        private const int INIT_NUM_OF_SETS = 5;
+        private const int INIT_NUM_OF_REPS = 15;
+        private const int MIN_COMMON = 1;
+
+        private AddNewExercisePageViewModel _viewModel;
+
         public AddNewExercisePage()
         {
             BuildLocalizedApplicationBar();
             InitializeComponent();
 
-            // fill ListPicker
+            // filling ListPicker
             NewExIntensityListPicker.ItemsSource = Enum.GetValues(typeof(Intensity));
+
+            _viewModel = new AddNewExercisePageViewModel();
+            ControlsStartValuesInitialization();
+            DataContext = _viewModel;
+        }
+
+        private void ControlsStartValuesInitialization()
+        {
+            _viewModel.NumOfSets = INIT_NUM_OF_SETS;
+            _viewModel.NumOfReps = INIT_NUM_OF_REPS;
         }
 
         private void BuildLocalizedApplicationBar()
@@ -46,7 +65,90 @@ namespace FitAndGym.View
 
         private void saveChanges_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            // TODO
+
+
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
         }
+
+        #region Plus-Minus Of Sets&Reps Events
+
+        private void NewExNumOfSetsPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if(_viewModel.NumOfSets < MAX_NUM_OF_SETS)
+                ++(_viewModel.NumOfSets);
+        }
+
+        private void NewExNumOfSetsMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (_viewModel.NumOfSets > MIN_COMMON)
+                --(_viewModel.NumOfSets);
+        }
+
+        private void NewExNumOfRepsPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (_viewModel.NumOfReps < MAX_NUM_OF_REPS)
+                ++(_viewModel.NumOfReps);
+        }
+
+        private void NewExNumOfRepsMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (_viewModel.NumOfReps > MIN_COMMON)
+                --(_viewModel.NumOfReps);
+        }
+
+        #endregion
+
+        #region CheckBox's Activation Events
+
+        private void NewExIntensityActiveCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked.Value)
+                NewExIntensityListPicker.IsEnabled = true;
+            else
+                NewExIntensityListPicker.IsEnabled = false;
+        }
+
+        private void NewExDurationActiveCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked.Value)
+                NewExDurationTimeSpanPicker.IsEnabled = true;
+            else
+                NewExDurationTimeSpanPicker.IsEnabled = false;
+        }
+
+        private void NewExSetsActiveCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked.Value)
+            {
+                NewExNumOfSetsTextBox.IsEnabled = true;
+                NewExNumOfSetsPlus.IsEnabled = true;
+                NewExNumOfSetsMinus.IsEnabled = true;
+            }
+            else
+            {
+                NewExNumOfSetsTextBox.IsEnabled = false;
+                NewExNumOfSetsPlus.IsEnabled = false;
+                NewExNumOfSetsMinus.IsEnabled = false;
+            }
+        }
+
+        private void NewExDurationRepsCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked.Value)
+            {
+                NewExNumOfRepsTextBox.IsEnabled = true;
+                NewExNumOfRepsPlus.IsEnabled = true;
+                NewExNumOfRepsMinus.IsEnabled = true;
+            }
+            else
+            {
+                NewExNumOfRepsTextBox.IsEnabled = false;
+                NewExNumOfRepsPlus.IsEnabled = false;
+                NewExNumOfRepsMinus.IsEnabled = false;
+            }
+        }
+
+        #endregion
     }
 }
---
+++ FitAndGym/ViewModels/AddNewExercisePageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitAndGym.ViewModels
+{
+    public class AddNewExercisePageViewModel : INotifyPropertyChanged
+    {
+        public AddNewExercisePageViewModel()
+        {
+
+        }
+
+        private int _numOfReps;
+        public int NumOfReps
+        {
+            get { return _numOfReps; }
+            set
+            {
+                _numOfReps = value;
+                NotifyPropertyChanged("NumOfReps");
+            }
+        }
+
+        private int _numOfSets;
+        public int NumOfSets
+        {
+            get { return _numOfSets; }
+            set
+            {
+                _numOfSets = value;
+                NotifyPropertyChanged("NumOfSets");
+            }
+        }
+
+        #region Events Stuff
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
