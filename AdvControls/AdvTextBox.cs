f2c024a15c588860cc5662cc10119ae16f8dc934
Szymon - Desktop <szymbar099@student.polsl.pl>
2013-11-17 13:58:55 +0100

    default text by AdvTextBox project added

---
+++ AdvControls/AdvTextBox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AdvControls
+{
+    public class AdvTextBox : TextBox
+    {
+        public static readonly DependencyProperty DefaultTextProperty = DependencyProperty.Register("DefaultText", typeof(string), typeof(AdvTextBox), new PropertyMetadata(""));
+
+        public string DefaultText
+        {
+            get
+            {
+                return (string)GetValue(DefaultTextProperty);
+            }
+            set
+            {
+                SetValue(DefaultTextProperty, value);
+                SetDefaultText();
+            }
+        }
+
+        public AdvTextBox()
+        {
+            this.GotFocus += (sender, e) =>
+            { if (this.Text.Equals(DefaultText)) { this.Text = String.Empty; } };
+            this.LostFocus += (sender, e) => { SetDefaultText(); };
+            this.Loaded += (sender, e) => { SetDefaultText(); };
+        }
+
+        private void SetDefaultText()
+        {
+            if (this.Text.Trim().Length == 0)
+            {
+                this.Text = DefaultText;
+            }
+        }
+    }
+}
