ad7217d16669bcdf68cb22f89f9f47eab5b56a33
Szymon - Desktop <szymbar099@student.polsl.pl>
2013-11-18 22:35:13 +0100

    pulling IValidableModel (problem with last commit)

---
+++ FitAndGym/ViewModels/IValidableModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FitAndGym.ViewModels
+{
+    public delegate void ValidationErrorEventHandler(object sender, ValidationErrorEventArgs e);
+
+    interface IValidableModel<T>
+    {
+        T GenerateModel();
+        event ValidationErrorEventHandler ValidationError;
+        string ToString();
+    }
+
+    public class ValidationErrorEventArgs
+    {
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+        }
+
+        public ValidationErrorEventArgs(string errorMessage)
+        {
+            _errorMessage = errorMessage;
+        }
+    }
+}
